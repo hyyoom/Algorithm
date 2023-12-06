b3a4766b6a04789805931bdd662070e086c796ab
hyyoo <96573766+hyungmin12@users.noreply.github.com>
2023-12-06 17:07:37 +0900

    [Gold IV] Title: 알파벳, Time: 7228 ms, Memory: 146452 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,34 +1,31 @@
 import sys
-
-# bfs 풀이
-# bfs
-def bfs():
-    global cnt
-    queue = set([(0, 0, graph[0][0])]) # 시간 초과를 줄이기 위해 중복되는 곳은 제거
-
-    while queue:
-        x, y, z = queue.pop()
-
-        # 말이 지날 수 있는 최대의 칸 초기화
-        cnt = max(cnt, len(z))
-
-        # 상/하/좌/우 탐색
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            # 범위 내에 있고 알파벳이 중복이 안된다면 탐색
-            if 0 <= nx < r and 0 <= ny < c and graph[nx][ny] not in z:
-                queue.add((nx, ny, graph[nx][ny] + z))
+# sys.setrecursionlimit(10**9)
 
 
-r, c = map(int, sys.stdin.readline().split())
+N,M = map(int,input().split())
+maxv = -1
 
-graph = [list(map(str, sys.stdin.readline().strip())) for _ in range(r)]
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-cnt = 1
+def dfs(y,x,cnt):
+    # global alpha
+    global maxv
+    
+    maxv = max(maxv,cnt)
+    for k in range(4):
+        ny = dy[k] + y
+        nx = dx[k] + x
+        if 0<=ny<N and 0<=nx<M and not maps[ny][nx] in alpha:
+            alpha.add(maps[ny][nx])
+            dfs(ny,nx,cnt+1)
+            alpha.remove(maps[ny][nx])
 
-bfs()
-print(cnt)
+maps = []
+dy = [-1,1,0,0]
+dx = [0,0,-1,1]
+for _ in range(N):
+    maps.append(list(input()))
+alpha = set()
+alpha.add(maps[0][0])
+
+dfs(0,0,1)
+print(maxv)
