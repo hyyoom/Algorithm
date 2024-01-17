af4298164f614a2be92904919b7668aaa3d1d664
hyyoo <96573766+hyungmin12@users.noreply.github.com>
2024-01-17 17:02:36 +0900

    [Silver III] Title: N과 M (1), Time: 292 ms, Memory: 36580 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,30 @@
+N, M = map(int, input().split())
+
+nums = [x for x in range(1, N+1)]
+
+
+v = [0] * (N+1)
+answer = []
+
+def solve(idx, target, result):
+    global v
+    global nums
+    global answer
+
+    if idx == target:
+        answer.append(result)
+        return
+    for i in range(N):
+        if nums[i] not in result:
+            v[idx] = 1
+            solve(idx+1, target, result+[nums[i]])
+            v[idx] = 0
+
+solve(0,M,[])
+
+for anw in answer:
+    for a in anw:
+        print(a,end=" ")
+    print()
+
+
