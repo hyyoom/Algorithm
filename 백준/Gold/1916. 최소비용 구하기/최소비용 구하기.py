8898d9a26d2dad4a40b792118563ca5455feb9fb
hyyoom <96573766+hyyoom@users.noreply.github.com>
2024-06-09 15:34:16 +0900

    [Gold V] Title: 최소비용 구하기, Time: 304 ms, Memory: 57372 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,26 +1,34 @@
-import heapq
 import sys
+from heapq import heappop, heappush
+
 input = sys.stdin.readline
 
-N = int(input())
-M = int(input())
-bus = [[] for _ in range(N+1)]
-for _ in range(M):
-    a,b,c = map(int,input().split())
-    bus[a].append((b,c))
-start, end = map(int,input().split())
+V = int(input())
+E = int(input())
 
-weight = [0xffffffff]*(N+1)
-weight[start] = 0
-q = []
-heapq.heappush(q,(start,0))
-while q:
-    node, w = heapq.heappop(q)
-    if weight[node] < w:
+weights = [sys.maxsize]*(V+1)
+nodes = [[] for _ in range(V+1)]
+
+for _ in range(E):
+    s,e,w = map(int, input().split())
+    nodes[s].append((w, e))
+
+start, end = map(int, input().split())
+
+heap = []
+weights[start] = 0
+heappush(heap, (0, start))
+
+while heap:
+    w, v = heappop(heap)
+    if weights[v] < w:
         continue
-    for next_node, next_w in bus[node]:
-        all_w = w + next_w
-        if all_w < weight[next_node]:
-            weight[next_node] = all_w
-            heapq.heappush(q, (next_node, all_w))
-print(weight[end])
+    for nw, nv in nodes[v]:
+        new_weight = w+nw
+        if new_weight < weights[nv]:
+            weights[nv] = new_weight
+            heappush(heap, (new_weight, nv))
+
+print(weights[end]) 
+    
+
